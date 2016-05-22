d5d1ad9e21bf3e7005ddaf6985047d610993f372
gcaro <gfcaro@hotmail.com>
2016-05-22 17:27:59 +0000

    add stocks and ajax fo UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   #get 'welcome/index'
 
@@ -6,7 +7,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'users#my_portfolio'
+  #root 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
